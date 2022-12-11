232f8a7dd8eb284f00d67e639b9c7415f1236ae0
guslipkin <gus.lipkin@gmail.com>
2022-12-10 23:37:28 -0500

    Added a preserve data option for follow_directions

--- R/follow_directions.R
+++ R/follow_directions.R
@@ -4,26 +4,35 @@
 #'   generate from a data.frame of direction and magnitude.
 #' @param row The starting row value
 #' @param col The starting column value
+#' @param preserve_data A boolean to indicate if data besides `row` and `col`
+#'   should be returned.
 #'
 #' @return A data.frame of coordinate pairs with `row` and `col` values.
 #' @export
-follow_directions <- function(.data, row, col) {
+follow_directions <- function(.data, row, col, preserve_data = FALSE) {
   dt <- .data
-  lapply(1:length(dt), \(i) {
-    dir <- dt[i]
+  re_pete <- ifelse(is.atomic(dt), length(dt), nrow(dt))
 
-    if (dir == "R") { coords <- list("row" = row, "col" = col + 1) }
-    else if (dir == "L") { coords <- list("row" = row, "col" = col - 1) }
-    else if (dir == "U") { coords <- list("row" = row - 1, "col" = col) }
-    else if (dir == "D") { coords <- list("row" = row + 1, "col" = col) }
+  df <-
+    lapply(1:re_pete, \(i) {
+      dir <- ifelse(is_atomic(dt), dt[i], dt[i,"dir"])
 
-    row <<- coords$row
-    col <<- coords$col
+      if (dir == "R") { coords <- list("row" = row, "col" = col + 1) }
+      else if (dir == "L") { coords <- list("row" = row, "col" = col - 1) }
+      else if (dir == "U") { coords <- list("row" = row - 1, "col" = col) }
+      else if (dir == "D") { coords <- list("row" = row + 1, "col" = col) }
 
-    return(list("row" = row, "col" = col))
-  }) |>
-    unlist(recursive = TRUE) |>
-    matrix(ncol = 2, byrow = TRUE) |>
-    `colnames<-`(c("row", "col")) |>
-    data.frame()
+      row <<- coords$row
+      col <<- coords$col
+
+      return(list("row" = row, "col" = col))
+    }) |>
+      unlist(recursive = TRUE) |>
+      matrix(ncol = 2, byrow = TRUE) |>
+      `colnames<-`(c("row", "col")) |>
+      data.frame()
+
+  if (preserve_data) { df <- cbind(dt,df) }
+
+  return(df)
 }
