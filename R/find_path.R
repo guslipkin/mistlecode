4239791a45098f055422afa92d67b1288c9af140
guslipkin <gus.lipkin@gmail.com>
2023-03-12 09:54:37 -0400

    This should do it

---
+++ R/find_path.R
@@ -0,0 +1,31 @@
+#' find_path
+#'
+#' @param .data
+#' @param start
+#' @param end
+#' @param type
+#'
+#' @return
+#' @export
+find_path <- function(.data, start, end, type) {
+  if (type == "mincost") {
+    return(.min_cost(.data, end[1], end[2]))
+  }
+}
+
+.min_cost <- function(.data, row, col) {
+  if (row < 1 | col < 1) { return(Inf) }
+  else if (row == 1 & col == 1) { return(.data[row,col]) }
+  else {
+    cost <- .data[row, col] + min(
+      .min_cost(.data, row - 1, col - 1),
+      .min_cost(.data, row - 1, col),
+      .min_cost(.data, row, col - 1)
+    )
+    return(cost)
+  }
+}
+
+.lee <- function(.data, row, col) {
+
+}
