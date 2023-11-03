8c8f01f32e171d469c922c16aea6a217db790fc0
guslipkin <gus.lipkin@gmail.com>
2023-11-02 21:45:57 -0400

    new function for making days

---
+++ R/make_day.R
@@ -0,0 +1,18 @@
+#' Make a File for a New Day
+#'
+#' @param year The year
+#' @param day The day
+#'
+#' @return Nothing
+#' @export
+make_day <- function(year = lubridate::year(Sys.Date()), day = lubridate::mday(Sys.Date())) {
+  day <- sprintf("%02d", day)
+  folder_path <- glue::glue('{year}/{day}')
+  if (!dir.exists(folder_path)) dir.create(folder_path, recursive = TRUE)
+  e <- rlang::current_env()
+  # readLines('data/template.qmd') -> template
+  mistlecode::template |>
+    sapply(glue::glue, .open = '{{', .close = '}}', .envir = e) |>
+    paste(collapse = '\n') |>
+    writeLines(glue::glue('{folder_path}/{year}_{day}.qmd'))
+}
